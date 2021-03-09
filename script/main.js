e81657b8d8f81d0f7ce44f9246a5f8d7469fa151
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2021-03-10 00:47:36 +0900

    210310_notice(ing)

--- script/main.js
+++ script/main.js
@@ -1,5 +1,28 @@
+
+
 $(".sub_gnb_inner>ul>li").hover(function(){
     $(this).siblings().children('.gnb_sub_wrap').css({display: 'none'});
     $(this).children('.gnb_sub_wrap').stop().slideToggle(400);
-
 })
+
+
+news_slide();
+
+
+function news_slide(){
+
+    let move = $(".news_rolling>li").height();
+    var news = 6;
+    let cnt = $(".news_rolling>li").length-1;
+    function rolling(){
+
+        $(".news_rolling>li").eq(1).animate({ 'top' : 0 }, 400, function(){
+            
+            $(".news_rolling>li").eq(0).css({'top' : '21px'});
+            news+=1;          
+            $(".news_rolling>li").eq(0).insertAfter($(".news_rolling>li").eq(cnt));
+        });
+    }
+    var rolling_start = setInterval(rolling, 2500);
+}
+
